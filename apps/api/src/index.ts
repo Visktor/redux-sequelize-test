57fd50e5a7328e5cde70b60a641b768d7418899a
virsto <webdev.vitor@gmail.com>
2024-08-05 20:03:21 -0300

    Adds logging

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,26 @@
 import express from "express";
 import { appRouter } from "./routes";
 import cors from "cors";
+import logger from "./logger";
+import { env } from "./lib/dotenv";
+import { sequelize } from "./lib/sequelize/config";
+
+const port = env.PORT;
+
+enum ExitStatus {
+  success,
+  failure,
+}
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error(`Unhandled promise rejection: ${promise}. Reason: ${reason}`);
+  throw reason;
+});
+
+process.on("uncaughtException", (error) => {
+  logger.error(`Uncaught promise exception: ${error}`);
+  process.exit(ExitStatus.failure);
+});
 
 const app = express();
 
@@ -24,4 +44,40 @@ app.use(
 );
 app.use(appRouter);
 
-app.listen(3000);
+app.listen(port, () => {
+  try {
+    logger.info(`Server running on http://localhost:${port}`);
+    const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
+
+    exitSignals.map((signal) =>
+      process.on(signal, async () => {
+        try {
+          logger.info("Server successfully disconnected");
+          process.exit(ExitStatus.success);
+        } catch (error) {
+          logger.error(`Error while disconnecting server ${error}`);
+          process.exit(ExitStatus.failure);
+        }
+      }),
+    );
+  } catch (error) {
+    logger.error("Server initialization error: " + error);
+    process.exit(ExitStatus.failure);
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .then(() => {
+    sequelize
+      .sync({ alter: true })
+      .then(() => console.log("All tables synchronized."))
+      .catch((err) => {
+        console.error(`Unable to synchronize tables. Error: ${err}`);
+      });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+    process.exit(1);
+  });
