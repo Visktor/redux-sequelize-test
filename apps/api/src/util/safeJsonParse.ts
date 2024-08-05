a5ffe39079729e66e9bf43a980ef314ce6764fb0
virsto <webdev.vitor@gmail.com>
2024-08-05 20:02:59 -0300

    Adds utility function safeJsonParse

---
+++ apps/api/src/util/safeJsonParse.ts
@@ -0,0 +1,16 @@
+import logger from "#/logger";
+
+export function safeJsonParse(thing: any): any | null {
+  try {
+    const parsed = JSON.parse(thing);
+
+    if (parsed === null || parsed === false || parsed === 0 || !!parsed) {
+      return parsed;
+    }
+
+    return null;
+  } catch (err) {
+    logger.error(`Safe JSON parser error: ${err}`);
+    return null;
+  }
+}
