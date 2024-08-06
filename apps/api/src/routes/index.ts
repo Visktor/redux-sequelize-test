60f1b384e1a0fdc5c9a7127e0161aaa65487d9cd
virsto <webdev.vitor@gmail.com>
2024-08-05 22:38:51 -0300

    Creates mock route get all users

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,7 +1,23 @@
+import { zodHandler } from "#/lib/zod/handler";
 import { Router } from "express";
+import { z } from "zod";
 
 const appRouter = Router();
 
 appRouter.get("/", async (_, res) => res.status(200).send("hello world"));
+
+appRouter.get(
+  "/users/getAll",
+  zodHandler({
+    schema: {
+      query: {
+        seila: z.string(),
+      },
+    },
+    handler: async (req, res) => {
+      const { seila } = req.query;
+    },
+  }),
+);
 
 export { appRouter };
