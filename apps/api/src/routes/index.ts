5f9edec4df79bf36f91ec998debbe53682a23b32
virsto <webdev.vitor@gmail.com>
2024-08-17 16:35:12 -0300

    Implements login route

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,14 +1,28 @@
-import { UserService } from "#/services/UserService";
+import { zodHandler } from "#/lib/zod/handler";
+import { LoginService } from "#/services/LoginService";
 import { Router } from "express";
+import { z } from "zod";
 
 const appRouter = Router();
 
-appRouter.get("/", async (_, res) => res.status(200).send("hello world"));
+appRouter.post(
+  "/login",
+  zodHandler({
+    schema: {
+      body: {
+        email: z.string().email(),
+        password: z.string().min(6),
+      },
+    },
+    handler: async (req, res) => {
+      const { email, password } = req.body;
 
-appRouter.get("/users/getAll", async (_, res) => {
-  const userService = new UserService();
-  const result = await userService.getMany();
-  res.status(result.success ? 200 : 500).json(result);
-});
+      const service = new LoginService();
+      const result = await service.login(email, password);
+
+      return res.status(result.success ? 200 : 500).json(result);
+    },
+  }),
+);
 
 export { appRouter };
